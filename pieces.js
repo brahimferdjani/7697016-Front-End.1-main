1895b20a2f1c4ef7606b7023e1b4cdb9731ca854
brahimferdjani <70799180+brahimferdjani@users.noreply.github.com>
2024-02-12 09:51:07 -0600

    added 2 addEeventListeners 1 for prix decroissant 2 for filtrer par description

--- pieces.js
+++ pieces.js
@@ -46,3 +46,21 @@ boutonTrier.addEventListener("click", function () {
         return piece.prix <= 35;
     });
    });
+
+const boutonTrierDecroissant = document.querySelector(".btn-trier-decroissant");
+
+boutonTrierDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     console.log(piecesOrdonnees);
+ });
+
+ const boutonFiltrerDescription = document.querySelector(".btn-trier-description");
+
+ boutonFiltrerDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description;
+    });
+   });
